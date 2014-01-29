629a4739a205bd50ec20d514d87fbf01733e0e62
Elliott Marquez <marquez@mit.edu>
2014-01-29 01:00:40 -0500

    Fixed the time issue and added comments

--- src/Server/Data.java
+++ src/Server/Data.java
@@ -2,17 +2,29 @@ package Server;
 
 import java.util.ArrayList;
 
+/**
+ * This class acts as a database that stores the points and data and time. It
+ * also has the ability to export all of the data into a CSV.
+ * 
+ * @author marquez
+ * 
+ */
 public class Data {
+    // Recording boolean
     private boolean recording = false;
+    // Live data arrays from fader and axes
     private ArrayList<Float[]> faderData = new ArrayList<Float[]>();
     private ArrayList<Float[]> axesData = new ArrayList<Float[]>();
+    // Array list of older datasets
     private ArrayList<ArrayList<Float[]>> previousCollections = new ArrayList<ArrayList<Float[]>>();
-    private Float initTime = null;
+    // Initial time in seconds since record button was pressed.
+    private Long initTime = null;
 
     /**
      * Begins storing data into the arrays
      */
     public void start() {
+        // Starts recording
         recording = true;
     }
 
@@ -25,10 +37,14 @@ public class Data {
      *            coming from the axes. False if it is coming from the fader.
      */
     public void stop(boolean axes) {
+        // Stops recording
         recording = false;
 
+        // Initializes the clone
         ArrayList<Float[]> clone = new ArrayList<Float[]>();
 
+        // Clones the axes data and adds it to the previous collections
+        // ArrayList
         if (axes) {
             try {
                 clone = (ArrayList<Float[]>) axesData.clone();
@@ -38,7 +54,11 @@ public class Data {
             }
 
             previousCollections.add(clone);
+            // Clears the axes data array
             axesData = new ArrayList<Float[]>();
+
+            // Clones the fader data and adds it to the previous collections
+            // ArrayList
         } else {
             try {
                 clone = (ArrayList<Float[]>) faderData.clone();
@@ -48,10 +68,14 @@ public class Data {
             }
 
             previousCollections.add(clone);
+            // Clears the fader data array
             faderData = new ArrayList<Float[]>();
         }
-        
+
+        // Prints everything for debugging
+        // TODO: REMOVE AFTER DEBUGGING IS FINISHED
         printall();
+        // Resets the initTime
         initTime = null;
     }
 
@@ -63,11 +87,14 @@ public class Data {
      *            represented as {1.0, 2.0} only two dimensions supported
      */
     public void addPoint(Float x, Float y) {
+        // Closes if not recording
         if (!recording)
             return;
 
+        // Sets the elapsed time
         float elapsedTime = firstCheck();
 
+        // Adds the appropriate data into the axes ArrayList
         axesData.add(new Float[] { elapsedTime, x, y });
     }
 
@@ -82,11 +109,14 @@ public class Data {
      *            This is the value of the fader as a Float value
      */
     public void addFaderDatum(float faderNumber, Float faderValue) {
+        // Closes if not recording
         if (!recording)
             return;
 
-        Float elapsedTime = firstCheck();
+        // Sets the elapsed time
+        float elapsedTime = firstCheck();
 
+        // Adds the appropriate data into the fader ArrayList
         faderData.add(new Float[] { elapsedTime, faderNumber, faderValue });
     }
 
@@ -96,23 +126,30 @@ public class Data {
      * @return The elapsed time
      */
     private float firstCheck() {
-        if (initTime == null) {
-            initTime = (float) System.currentTimeMillis();
+        // Sets the initial time if null
+        if (this.initTime == null) {
+            this.initTime = System.currentTimeMillis();
         }
 
-        return (float) (System.currentTimeMillis() - initTime);
+        // finds the elapsed time in seconds
+        float elapsed = ((float) ((System.currentTimeMillis() % 1000000 - this.initTime % 1000000)) / 1000);
+
+        // returns the correct elapsed time if it clocks over 1000000ms
+        return elapsed > 0 ? elapsed : elapsed + 1000;
     }
 
+    // PRINTS EVERYTHING
+    // TODO: REMOVE AFTER DEBUGGING
     private void printall() {
         for (int i = 0; i < previousCollections.size(); i++) {
             ArrayList<Float[]> collection = previousCollections.get(i);
             if (collection.size() == 0)
                 break;
-            
+
             for (int j = 0; j < collection.size(); j++) {
                 Float[] data = collection.get(j);
                 String output = "";
-                
+
                 for (int k = 0; k < data.length; k++) {
                     output += data[k] + "\t";
                 }
--- src/Server/MessageListener.java
+++ src/Server/MessageListener.java
@@ -7,35 +7,58 @@ import com.illposed.osc.OSCMessage;
 import com.illposed.osc.OSCPortIn;
 
 public class MessageListener implements OSCListener {
+    // This is the database to be accessed
     private final Data database;
 
+    /**
+     * Constructs the object and uses the given database
+     * 
+     * @param database
+     *            This is the database to be accessed by the listener.
+     */
     public MessageListener(Data database) {
         this.database = database;
     }
 
+    /**
+     * This is ran every time the OSCport receives an OSCMessage and it stores
+     * data into the database given an address of "/1/xy1", "/2/multifader1/#",
+     * "/1/toggle1", and "/2/toggle1"
+     */
     public void acceptMessage(Date executionTime, OSCMessage message) {
+        // Address of the message (aka component name)
         String address = message.getAddress();
+        // Data in an object array
         Object[] data = message.getArguments();
 
-        String[] addressComponents = address.split("/");
-        
+        // returns if the data is empty or null
         if (!(data.length != 0 && data[0] != null)) {
             return;
         }
 
+        // adds a point to the database
         if (address.equals("/1/xy1")) {
-            this.database.addPoint((Float) data[0], (Float) data [1]);
+            this.database.addPoint((Float) data[0], (Float) data[1]);
 
+            // adds the value to the database
         } else if (address.contains("/2/multifader1")) {
-            float faderNum = 0; 
+            // instantiates the fader number
+            float faderNum = 0;
+            
             try {
-                faderNum = Float.parseFloat((String) addressComponents[3]);
+                // finds the bar number
+                String barNumber = address.split("/")[3];
+                faderNum = Float.parseFloat(barNumber);
+                
             } catch (Exception e) {
                 System.out.println("There was an error parsing the multifader");
                 e.printStackTrace();
             }
+            
+            // adds the value to the database
             this.database.addFaderDatum(faderNum, (Float) data[0]);
 
+            // starts / stops the database collection for the axes
         } else if (address.equals("/1/toggle1")) {
             if ((Float) data[0] == 1f) {
                 this.database.start();
@@ -44,6 +67,7 @@ public class MessageListener implements OSCListener {
                 this.database.stop(true);
 
             }
+            // starts / stops the database collection for the faders
         } else if (address.equals("/2/toggle1")) {
             if ((Float) data[0] == 1f) {
                 this.database.start();
